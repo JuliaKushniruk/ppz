8fb19287696ba997ead723fa111d7d708f86f2ae
Barabashka88 <jasoba4ka@gmail.com>
2017-04-06 23:09:43 +0300

    Added Movie Controller and View

---
+++ WebSite/WebSite/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            Movie newMovie = new Movie();
+            newMovie.Name = "RoKi";
+            newMovie.Year = 1997;
+            newMovie.Genre = "Strange";
+            newMovie.Author = "Avildsen";
+            newMovie.Cast = new List<string> { "Stalone", "Vesers" };
+            newMovie.Description = "Cool movie";
+            return View("Movie" , newMovie);
+        }
+    }
+}
