b008364c9fac40a9998a98062eeee7b4d387b199
Юра Неклеса < >
2017-04-06 22:20:59 +0300

    Added initial classes for cinema

---
+++ WebSite/WebSite/Models/Cinema.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace WebSite.Models
+{
+    public class Cinema
+    {
+        private string name;
+        private string description;
+        private string address;
+        private Seats seats;
+        private List<string> events; //string = Event
+    }
+
+    public class Seats
+    {
+        private int rows;
+        private int columns;
+
+        public Seats(int _rows, int _columns)
+        {
+            rows = _rows;
+            columns = _rows;
+        }
+
+        public int Rows { get { return rows; } }
+        public int Columns { get { return columns; } }
+        public int Count {  get { return rows * columns; } }
+    }
+}
