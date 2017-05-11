dac59d890d41671c03599ca8dfa454294fb51775
JuliaKushniruk <julzzzzzz807@gmail.com>
2017-05-11 21:23:04 +0300

    Fixed registration page crash

--- WebSite/Domain/Entities/Ticket.cs
+++ WebSite/Domain/Entities/Ticket.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities
 {
     public class Ticket
     {
+        [Key]
+        public int TicketId { get; set; }
         public Event Event { get; set; }
         public AppUser Owner { get; set; }
         public int Row { get; set; }
