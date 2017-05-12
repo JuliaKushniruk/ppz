1211af0228f0a6d4b8cf2b3c4d72c4f350f88a34
JuliaKushniruk <julzzzzzz807@gmail.com>
2017-05-12 03:34:26 +0300

    Added search for events

---
+++ WebSite/WebSite/Controllers/SearchEventsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.Models;
+using Domain.Concrete;
+using Domain.Entities;
+
+namespace WebSite.Controllers
+{
+    public class SearchEventsController : Controller
+    {
+        private MainRepository repository = new MainRepository();
+
+        [HttpGet]
+        public ActionResult SearchEvents()
+        {
+            SearchEventModel model = new SearchEventModel();
+            model.EventsFound = from ev in repository.GetEvents()
+                                select ev;
+            return View("Search", model);
+        }
+
+        [HttpPost]
+        public ActionResult SearchEvents(SearchEventModel model)
+        {
+            model.EventsFound = from ev in repository.GetEvents()
+                                where ev.Name.ToLower().Contains(model.Name.ToLower())
+                                select ev;
+            return View("Search", model);
+        }
+    }
+}
---
+++ WebSite/WebSite/Models/SearchEventModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain.Entities;
+
+namespace WebSite.Models
+{
+    public class SearchEventModel
+    {
+        public string Name { get; set; }
+        public IEnumerable<Event> EventsFound { get; set; }
+    }
+}
